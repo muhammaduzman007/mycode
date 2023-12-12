9e4f32bbc867915a46d80ab36f4648bb60f632ee
Uzman <student@bchd.72a799be-b6a3-4e26-af58-19e281a09ff3>
2023-12-12 10:03:36 +0000

    terraform and variables

---
+++ vars-tf/variables.tf
@@ -0,0 +1,22 @@
+variable "container_name" {
+  description = "Value of the name for the Docker container"
+  # basic types include string, number and bool
+  type        = string
+  default     = "ExampleNginxContainer"
+}
+
+
+variable "internal" {
+  description = "Value of the name for the Docker container"
+  # basic types include string, number and bool
+  type        = number
+  default     = 80
+}
+
+
+variable "external" {
+  description = "Value of the name for the Docker container"
+  # basic types include string, number and bool
+  type        = number
+  default     = 2224
+}
